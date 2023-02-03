769a7164d8d5d01176af429df57df2f88d74ccdd
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-03 18:21:46 +0600

    Total Price

--- Total-Price.js
+++ Total-Price.js
@@ -1,19 +1,20 @@
 
 
 const shoppigItems = [
-    { name: 'shirt', price: 2600 },
-    { name: 'pant', price: 2000 },
-    { name: 'tShirt', price: 1600 },
-    { name: 'shoe', price: 3000 },
-    { name: 'cap', price: 1400 }
+    { name: 'shirt', price: 2600, quantity: 3 },
+    { name: 'pant', price: 2000, quantity: 2 },
+    { name: 'tShirt', price: 1600, quantity: 4 },
+    { name: 'shoe', price: 3000, quantity: 2 },
+    { name: 'cap', price: 1400, quantity: 4 }
 ]
 
 function totalExpense(items) {
     let sum = 0;
     for (i = 0; i < items.length; i++) {
         let item = items[i];
-        let price = item.price;
-        sum = sum + price;
+        let poriman = item.quantity;
+        let total = item.price * poriman;
+        sum = sum + total;
     }
     return sum;
 }
